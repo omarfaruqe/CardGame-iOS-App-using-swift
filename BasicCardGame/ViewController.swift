ad45eaaed17c2cc3b279bf51a55db0ae2b782fc8
Omar Faruqe <faruqe@Omars-MacBook-Pro.local>
2015-06-01 15:40:13 -0400

    Initial Commit

---
+++ BasicCardGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicCardGame
+//
+//  Created by Omar Faruqe on 2015-06-01.
+//  Copyright (c) 2015 Omar Faruqe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
